321768166ed955fb225c871a4137834df19a6f09
michellethemaker <83628697+michellethemaker@users.noreply.github.com>
2021-10-21 01:04:06 +0800

    Update __init__.py

--- custom_blemesh/__init__.py
+++ custom_blemesh/__init__.py
@@ -7,7 +7,7 @@ register(
 
 register(
      id='customblemesh-v0.1',
-     entry_point='customblemesh.envs:customblemeshEnv'
-     max_episode_steps=1500
+     entry_point='customblemesh.envs:customblemeshEnv',
+     max_episode_steps=1500,
      kwargs={'size' : 1, 'init_state' : 10., 'state_bound' : np.inf},
  )
