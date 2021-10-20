c61ca1cce1d71655becc812033922d84abab80c0
michellethemaker <83628697+michellethemaker@users.noreply.github.com>
2021-10-21 00:52:15 +0800

    Add files via upload

---
+++ custom_blemesh/__init__.py
@@ -0,0 +1,13 @@
+from gym.envs.registration import register
+
+register(
+     id='customblemesh-v0',
+     entry_point='customblemesh.envs:customblemeshEnv'
+ )
+
+register(
+     id='customblemesh-v0.1',
+     entry_point='customblemesh.envs:customblemeshEnv'
+     max_episode_steps=1500,
+     kwargs={'size' : 1, 'init_state' : 10., 'state_bound' : np.inf},
+ )
---
+++ custom_blemesh/envs/__init__.py
@@ -0,0 +1,1 @@
+from custom_blemesh.envs.customblemeshEnv import customblemeshEnv
